919e8d90e3afeaca5c59b3e47e6661aaedebeccc
홍진우 <whatar@naver.com>
2022-04-09 21:05:22 +0900

    배운것 연습/실습

---
+++ shoppingmall 연습/WebContent/main.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<%
+	String mesg = (String)session.getAttribute("memberAdd");
+	if(mesg!=null){
+%>
+<script type="text/javascript">
+	alert("<%=mesg%>");
+</script>
+<%
+	session.removeAttribute("memberAdd");
+	}
+%>
+</head>
+<body>
+<h1>메인화면입니다.</h1>
+<jsp:include page="common/top.jsp" flush="true"></jsp:include><br>
+<jsp:include page="common/menu.jsp" flush="true"></jsp:include>
+</body>
+</html>
---
+++ shoppingmall 연습/src/com/controller/member/MemberIdCheckServlet.java
@@ -0,0 +1,41 @@
+package com.controller.member;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.service.MemberService;
+
+/**
+ * Servlet implementation class MemberIdCheckServlet
+ */
+@WebServlet("/MemberIdCheckServlet")
+public class MemberIdCheckServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public MemberIdCheckServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String userid = request.getParameter("userid");
+		System.out.println(userid);
+		
+		MemberService service = new MemberService();
+		int count = service.idCheck(userid);
+		System.out.println(count);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
